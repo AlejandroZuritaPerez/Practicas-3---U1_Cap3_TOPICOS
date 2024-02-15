28314e2ce1271cbe81150ed9b83c4700d91e15c6
AlejandroZuritaPerez <119274449+AlejandroZuritaPerez@users.noreply.github.com>
2024-02-15 15:46:39 -0600

    Add files via upload

---
+++ Ejercicio2_Cap3_WinForm/Ejercicio2_Cap3_WinForm/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Ejercicio2_Cap3_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(144, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(511, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CONVERTIDOR DE TEMPERATURAS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label3.Location = new System.Drawing.Point(58, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(342, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "¿Ingrese el valor de la temperatura en Grados?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBox1.Location = new System.Drawing.Point(474, 253);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 26);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "GRADOS",
+            "RADIANES"});
+            this.comboBox1.Location = new System.Drawing.Point(474, 157);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(181, 28);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "Seleccione una Opción...";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label4.Location = new System.Drawing.Point(71, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(329, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "¿Que Tipo De Temperatura Desea Convertir?";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(474, 317);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 26);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.AutoSize = true;
+            this.labelResultado.Location = new System.Drawing.Point(513, 368);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(108, 20);
+            this.labelResultado.TabIndex = 7;
+            this.labelResultado.Text = "RESULTADO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 323);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(366, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "¿Ingrese el valor de la temperatura en Fahrenheit?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Ejercicio2_Cap3_WinForm/Ejercicio2_Cap3_WinForm/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Ejercicio2_Cap3_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string respuesta = comboBox1.SelectedItem.ToString().Trim().ToLower();
+
+            if (respuesta == "grados")
+            {
+                double grados;
+                if (!double.TryParse(textBox1.Text, out grados))
+                {
+                    MessageBox.Show("Valor inválido. Por favor, ingrese un número válido.");
+                    return;
+                }
+                MostrarResultado(ConvertirRadianesAGrados(grados));
+            }
+            else if (respuesta == "radianes")
+            {
+                double radianes;
+                if (!double.TryParse(textBox2.Text, out radianes))
+                {
+                    MessageBox.Show("Valor inválido. Por favor, ingrese un número válido.");
+                    return;
+                }
+                MostrarResultado(ConvertirGradosARadianes(radianes));
+            }
+            else
+            {
+                MessageBox.Show("Respuesta no válida. Por favor, ingrese 'grados' o 'radianes'.");
+            }
+        }
+
+
+        private double ConvertirRadianesAGrados(double radianes)
+        {
+            return (radianes * 180) / Math.PI;
+        }
+
+        private double ConvertirGradosARadianes(double grados)
+        {
+            return (grados * Math.PI) / 180;
+        }
+
+        private void MostrarResultado(double resultado)
+        {
+            labelResultado.Text = resultado.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
